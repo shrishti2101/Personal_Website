c1bd78726a6d6b0b47b4f1be8abf10b48e1390eb
shrishtijain2101 <shrishtijain2101@gmail.com>
2024-03-27 20:29:43 +0000

    Hero title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom';
 import Hero from '../assets/photo.png'
 import './Home.scss'
+import { ReactTyped as Typed} from "react-typed";
+
+
 function Home() {
+
     return(
         <div className='hero-container'>
         <div className='hero'>
@@ -9,18 +13,20 @@ function Home() {
             <img src={Hero} alt="" />
         </div>
         <div className='hero-text'>
-            <h1>
-                I'm <span>Shrishti Jain</span>
-            </h1>
-            <h2>
-               Frontend Developer
-            </h2>
+            <h1 className='hero-title'>
+                I'm <span className="title">
+                <Typed strings={["Shrishti Jain","Frontend Developer"] }
+                typeSpeed={100} 
+                backSpeed={100} 
+                loop/>
+                </span></h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro earum ipsum ut veniam amet laudantium cupiditate tenetur deleniti aut? Amet velit blanditiis obcaecati et! Adipisci laboriosam nobis nulla optio voluptatem.</p>
             <Link to='/about'>Know More</Link>
         </div>
         </div>
         </div>
-    )
+    )    
+
 }
 
 export default Home;
